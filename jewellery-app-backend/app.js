eac5b4488efded2d888b54337c7b3c4ddc1574d6
Kaveesha Himasanka <himasanka@commercialtp.com>
2023-07-20 16:22:11 +0530

    create mongodb connection

--- jewellery-app-backend/app.js
+++ jewellery-app-backend/app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const customer = require('./routes/customer');
 const items = require('./routes/items');
+
 const app = express();
 const port = 4000;
+
+const url = 'mongodb://127.0.0.1/jewelleryshop';
+
+mongoose.connect(url, {useNewUrlParser: true});
+const con = mongoose.connection;
+
+con.on("open", () => {
+  console.log("MongoDB Connected...!")
+});
 
 app.use(express.json());
 app.use('/customer', customer);
