5fc8f77affb41be909105fe451aeca398eeb1d41
Kaveesha Himasanka <himasanka@commercialtp.com>
2023-07-20 16:01:32 +0530

    create routings

--- jewellery-app-backend/app.js
+++ jewellery-app-backend/app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const customer = require('./routes/customer');
+const items = require('./routes/items');
 const app = express();
 const port = 4000;
 
 app.use(express.json());
+app.use('/customer', customer);
+app.use('/items', items);
 
 app.get('/', (req, res) => {
-  console.log(req.body);
+  console.log(req.body.code);
   res.send('Hello World!')
 });
 
